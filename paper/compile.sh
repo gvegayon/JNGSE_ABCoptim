907cfb415418f2575e06d566287239cb7ed9287a
George Vega Yon <g.vegayon@gmail.com>
2014-05-01 15:37:45 -0400

    Ordenando un poco mas

--- paper/compile.sh
+++ paper/compile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Compila Los archivos con knitr
-Rscript -e 'knitr::knit("01_example1.Rnw")'
+Rscript -e 'knitr::knit("../R/01_example1.Rnw")'
 rm -f 01_example1.pdf
 
 # Compila el tex
 pdflatex '00_main.tex'
+
+# Abriendo
+evince '00_main.pdf' &
 
 # Limpia
 rm -f *.out
