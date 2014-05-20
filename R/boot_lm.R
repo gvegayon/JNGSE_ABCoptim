8c879a6101665977f47df27d92f2ec26ab039fb0
George Vega Yon <g.vegayon@gmail.com>
2014-05-20 00:07:04 -0400

    Adding TeX requirements +  some trials using bootstrapping OLS

---
+++ R/boot_lm.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+
+library(boot)
+
+dataset <- read.delim("../data/TEGDataAlirezabahadori.csv", sep=",")
+colnames(dataset) <- c("gasflowrate","temp","teg")
+
+fun <- function(data, indices) {
+	# Subsample
+	data <- data[indices,]
+	
+	# New indices for pick
+	N <- nrow(data)
+	n <-0
+	while(!n) {
+		pick <- which(runif(N) > .5)
+		n <- sum(pick)
+	}
+	# Test data
+	testdata   <- data[pick,]
+	predictdata <- data[!(1:N %in% pick),]
+
+	# Predicting model and measuring error
+	fit <- lm(teg~temp+gasflowrate, data=testdata)
+	newdata <- predict(fit, predictdata[,c("temp","gasflowrate")])
+
+#print(cbind(newdata,predictdata$teg))
+
+	return(mean(newdata-predictdata$teg))
+
+}
+
+bs <- boot(dataset, fun, R=500)
+bs
+summary(bs)
+
+plot(bs)
